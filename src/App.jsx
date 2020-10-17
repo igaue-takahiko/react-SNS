3947d4c1c44d428eb32e2f755fdae5aed7852c43
igaue-takahhiko <hasewo.2610@gmail.com>
2020-10-18 03:52:40 +0900

    created custom material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { createMuiTheme, } from '@material-ui/core'
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core'
+import indigo from '@material-ui/core/colors/indigo'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: indigo,
+        secondary: {
+            main: "#f44336",
+        }
+    },
+    typography: {
+        fontFamily: "Comic Neue"
+    }
+})
 
 const App = () => {
     return (
-        <div>
-        </div>
+        <MuiThemeProvider theme={theme}>
+        </MuiThemeProvider>
     );
 }
 
